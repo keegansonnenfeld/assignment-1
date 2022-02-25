fec3d97e70d60b80ceb6aacd5f64ea487f321f68
keegansonnenfeld <keegan.sonnenfeld@gmail.com>
2022-02-24 18:46:07 -0600

    problem 1

---
+++ pysics.py
@@ -0,0 +1,12 @@
+#calculating momentem
+def Momentum(m, v):
+    return (float(m) * float(v))
+
+#calc energy
+def KineticEnergy(m, v):
+    return (float(m) * float(v) * float(v))
+
+#inputs
+M = input("enter the Mass :")
+
+V = input("enter the Velocity :")
